3a3cf4968c5499c27390134020279dd373dfdb40
laijingquan <351779036@qq.com>
2018-03-23 14:34:36 +0800

    。。。

---
+++ Assets/Scripts/BallTriggerTest.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using TrueSync;
+namespace PoolEngine
+{
+    public class BallTriggerTest : TrueSyncBehaviour
+    {
+
+        // Use this for initialization
+        private TSVector moveDir = TSVector.right*-1;
+        public override void OnSyncedUpdate()
+        {
+            tsTransform.Translate(moveDir * Time.deltaTime);
+        }
+
+        public void OnSyncedTriggerEnter(TSCollision collision)
+        {
+            moveDir = collision.contacts[0].normal;
+        }
+    }
+}
