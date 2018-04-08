7c079316581a22aaf4b4189e082982f04c47925c
laijingquan <351779036@qq.com>
2018-04-08 11:04:00 +0800

    1

---
+++ Assets/Scripts/PoolEngine/Body.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using TrueSync;
+namespace PoolEngine
+{
+    public class Body
+    {
+        protected TSVector2 w;
+        protected TSVector2 v;
+        protected TSVector2 wx;
+        protected TSVector2 wy;
+        protected TSVector2 vx;
+        protected TSVector2 vy;
+
+        protected TSVector2 _w;
+        protected TSVector2 _v;
+        protected TSVector2 _wx;
+        protected TSVector2 _wy;
+        protected TSVector2 _vx;
+        protected TSVector2 _vy;
+
+        public void Active()
+        {
+
+        }
+
+        public void Reset()
+        {
+
+        }
+
+        public TSVector2 Move(TSVector2 moveDir)
+        {
+
+            return moveDir;
+        }
+    }
+}
